98f543ac1890dd4a0d02f5848e0387942c1db08d
calintje <calsim113@gmail.com>
2025-03-21 19:00:12 +0100

    Clean git stash

--- anchor-versions/anchor-v0.29.0/client/utils/constants.ts
+++
@@ -1,4 +0,0 @@
-import { address } from "@solana/kit";
-
-export const METADATA_UPDATE_AUTH = address("3axbTs2z5GBy6usVbNVoqEgZMng3vZvMnAoX29BFfwhr");
-export const SPLASH_POOL_TICK_SPACING = 32896;
